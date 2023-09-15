5675da88717e943a0008ce708a97f801f97ca696
nocared <paul.richarddu30@gmail.com>
2023-09-15 11:37:51 +0200

    Conventions en php

--- app/index.php
+++ app/index.php
@@ -24,13 +24,45 @@
 <?php
 
 
-// Variables
 
 //On déclare une variable avec $                      -> faire attention à la casse
 $variable = 'Une variable';
 
 $Variable = 'Une autre variable';
 
-echo $variable;
-echo '<br>';
-echo $Variable;
+////     Affichage des variables
+// echo $variable;
+// echo '<br>';
+// echo $Variable;
+
+
+
+// //DIfférentes façons d'écrire le résultat
+//$nb = '5';
+// echo $nb;
+//  echo '<br>';
+// printf($nb);
+//  echo '<br>';
+// var_dump($nb);
+
+
+
+// //Var dump affiche le type de la variable
+// $nb = 5;
+// var_dump($nb);
+
+
+
+//// Déclaration variable
+// $nb = (string) '5';
+// $nb = (int) '5';
+// $nb = (float) '5';
+// $nb = (bool) '5';
+// var_dump($nb);
+
+
+
+// $camelCase = 'Une variable en camelCase';
+// $snake_case = 'Une variable en snake_case';
+// $PascalCase = 'Une variable en PascalCase';
+
