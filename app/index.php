f58555c553532eeb6c2b673ac30108f83ebaea99
nocared <paul.richarddu30@gmail.com>
2023-09-15 10:29:04 +0200

    Ouverture et fermeture balise php sur un fichier

--- app/index.php
+++ app/index.php
@@ -6,3 +6,12 @@
 // ---------------------------------------------------------------------------------------------
 
 echo "test";
+
+//Balise fermante de php pour pouvoir faire du html par la suite
+?>
+
+<h1> php </h1>
+
+
+<!-- Si on veut refaire du php on rouvre une balise php -->
+<?php
