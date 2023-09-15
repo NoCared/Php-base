7c2b239ce0b7fc2f4f9590544814529efa0ce357
nocared <paul.richarddu30@gmail.com>
2023-09-15 11:15:20 +0200

    Premières variables

--- app/index.php
+++ app/index.php
@@ -5,13 +5,32 @@
 
 // ---------------------------------------------------------------------------------------------
 
-echo "test";
+// echo "test";
 
 //Balise fermante de php pour pouvoir faire du html par la suite
 ?>
 
-<h1> php </h1>
+<!-- <h1> php </h1> -->
 
+
+
+
+
+
+
+<!-- ----------------------------------------------------------------------------------- -->
 
 <!-- Si on veut refaire du php on rouvre une balise php -->
 <?php
+
+
+// Variables
+
+//On déclare une variable avec $                      -> faire attention à la casse
+$variable = 'Une variable';
+
+$Variable = 'Une autre variable';
+
+echo $variable;
+echo '<br>';
+echo $Variable;
