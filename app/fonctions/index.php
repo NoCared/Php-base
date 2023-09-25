7f53be863cec69e7e05335c498cb57c970d71174
nocared <paul.richarddu30@gmail.com>
2023-09-25 14:36:37 +0200

    Functions with strings

--- app/fonctions/index.php
+++ app/fonctions/index.php
@@ -1,22 +1,75 @@
 <?php
 
-function debug($value) 
+//Sert à provoquer des erreurs si le typage n'est pas bon
+declare(strict_types=1);
+
+function debug($value)
 {
     echo '<pre style="background-color:black; color:white;padding:15px;overflow:auto;">';
     print_r($value);
     echo '</pre>';
 }
 
-function dd($value) 
+function dd($value)
 {
     debug($value);
     die("----------------------------------- Stop function -----------------------------------");
 }
 
-function calcul($a,$b)
+function calcul(int $a, int $b): int
 {
-    $c = $a +$b;
-    return $c;
+    return $a + $b;
 }
 
-echo calcul(10,12);
+function calculAdd(int $a, int $b = 5): int
+{
+    return $a + $b;
+}
+
+// echo calcul(10, 12) . "<br>";
+// echo calculAdd(10) . "<br>";
+
+//----------
+
+
+//Faire une fonction qui calcule l'age d'une personne en lui passant la date de naissance
+//Faire une fonction qui calcule le prix TTC en lui passant le prix HT et la TVA
+function AgeSomeone(int $date) : int
+{
+    return date("Y") - $date;
+}
+function CalcPrixTTC(float $ht,float $tva) : float 
+{
+    return $ht + $ht * $tva/100;
+}
+
+// echo AgeSomeone(1999) . '<br>';
+// echo CalcPrixTTC(34,10);
+
+
+//----------
+
+
+// fonction interne à php
+$article = "Je lis un article.";
+
+//Inverser chaine de caractères
+//strrev
+//Longueur d'une chaine de caractère
+//strlen
+//Transforme une chaine de caractère en majuscule
+//strtouper
+//Remplace un élément dans une chaine de caractère
+// ----> remplace 'un' par 'cet'
+//str_replace
+//Remplace un segment dans une chaine de caractère
+// ----> renvoyer les 6 premiers éléments de la chaine de caractère
+//substr
+
+$article = str_replace('un','cet',$article,$count);
+
+echo $article . " " . $count . "<br>";
+
+$article = substr($article,0,6);
+
+echo $article . "<br>";
