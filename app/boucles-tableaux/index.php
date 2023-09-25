4c718b25568725a5140aaf039a537bd97fe45aa5
nocared <paul.richarddu30@gmail.com>
2023-09-25 16:31:37 +0200

    Exercices employés + parcours de tableaux

--- app/boucles-tableaux/index.php
+++ app/boucles-tableaux/index.php
@@ -1,3 +1,44 @@
 <?php
 require '../fonctions/index.php';
+?>
+<!DOCTYPE html>
+<html lang="en">
 
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Employees</title>
+</head>
+
+<body>
+    <?php
+        $jsonListEmployees = file_get_contents('../data/employees.json');
+
+        $jsonListEmployeesArray = json_decode($jsonListEmployees, true);
+        debug($jsonListEmployeesArray);
+
+        foreach($jsonListEmployeesArray['data'] as $key => $value)
+        {
+            echo "<ul>Number $key:<br>";
+            echo "<li>Name " . $value['employee_name'] . '</li>';
+            echo "<li>Salary " . $value['employee_salary'] . '</li>';
+            echo "<li>Age " . $value['employee_age'] . '</li>';
+            echo '</ul><br>';
+        }
+
+        //Manière avec les classes eheheh
+        $jsonListEmployeesArray = json_decode($jsonListEmployees);
+        foreach($jsonListEmployeesArray->data as $key => $value)
+        {
+            echo "<ul>Number $key:<br>";
+            echo "<li>Name " . $value->employee_name . '</li>';
+            echo "<li>Salary " . $value->employee_salary . '</li>';
+            echo "<li>Age " . $value->employee_age . '</li>';
+            echo '</ul><br>';
+        }
+
+
+    ?>
+</body>
+
+</html>
---
+++ app/data/villes.php
@@ -0,0 +1,22 @@
+<?php
+
+$villes = [
+    [
+        'nom' => 'Paris',
+        'codePostal' => '75000',
+        'population' => '2146902',
+        'siteWeb' => 'http://www.paris.fr'
+    ],
+    [
+        'nom' => 'Marseille',
+        'codePostal' => '13000',
+        'population' => '870145',
+        'siteWeb' => 'http://www.marseille.fr'
+    ],
+    [
+        'nom' => 'Lyon',
+        'codePostal' => '69000',
+        'population' => '522365',
+        'siteWeb' => 'http://www.lyon.fr'
+    ]
+];
