e24a1e4c44a0142b25a813303e8f7cc1d3edd18a
nocared <paul.richarddu30@gmail.com>
2023-09-25 10:15:21 +0200

    Ajout de toutes les fonctions pour itérer sur un tableau

--- app/boucles/index.php
+++ app/boucles/index.php
@@ -2,8 +2,39 @@
 require '../fonctions/index.php';
 
 
-$planetes = ['mars','uranus','jupiter','terre','saturne'];
+$planetes = ['mars', 'uranus', 'jupiter', 'terre', 'saturne'];
 
-dd($planetes);
+debug($planetes);
 
-echo 'test';
+foreach ($planetes as $planete) {
+    echo "$planete<br>";
+}
+
+echo '<br>';
+
+foreach ($planetes as $key => $planete) {
+    echo "$key : $planete<br>";
+}
+
+echo '<br>';
+
+for ($value = 0; $it < count($planetes);$it++)
+{
+    echo "$planetes[$it] <br>";
+}
+
+echo '<br>';
+
+$it = 0;
+while ($it < count($planetes))
+{
+    echo "$planetes[$it] <br>";
+    $it++;
+}
+
+echo '<br>';
+
+array_walk($planetes,function ($element){echo "$element<br>";} );
+
+$numbers = [2,3,4,5];
+dd($numbers);
