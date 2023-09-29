110cd49d8ad8b731a31e373471f66f1ee245efaf
nocared <paul.richarddu30@gmail.com>
2023-09-29 16:48:49 +0200

    ADD - Datas for blog database

--- app/database/data.sql
+++ app/database/data.sql
@@ -9,3 +9,8 @@ CREATE TABLE IF NOT EXISTS post
     niveau VARCHAR(255),
     createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
+
+INSERT INTO post (name,email,titre,commentaire,competence,niveau) VALUES
+('jon doe','jjo@yahoo.uk','titre','commentaire fait par jjo','php,python','intermediaire'),
+('jane doe','jjaaaaa@gamil.uk','titre de jane','blabla','python,html','debutant'),
+('bla blo','blabla@free.fr','blatitrebla','jai la flemme','python','expert');
